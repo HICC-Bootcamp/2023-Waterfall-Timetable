b758d3b47ffee0b9f62a5f2b37a78ebdc0c88ee8
insa658 <016andrew@g.hongik.ac.kr>
2023-01-19 19:58:43 +0900

    flask_server.py 수정

---
+++ flask_server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def initial():
+    return render_template('Initialpage.html')
+
+@app.route('/Teacherpage/1/')
+def teacher():
+    return render_template('Teacherpage_1.html')
+
+@app.route('/3/')
+def teacher_2():
+    return render_template('Teacherpage_2.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0", port="9999")
