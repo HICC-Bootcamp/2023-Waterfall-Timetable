cdf5fb74b8cc01709255af7d804a0af3f7d098cf
insa658 <016andrew@g.hongik.ac.kr>
2023-01-29 18:03:23 +0900

    manager_result 주석

--- templates/test.py
+++ templates/test.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import openpyxl
 filename = 'C:/Users/user/Desktop/올빼미/excel_saver/manage_attendance.xlsx'
+<<<<<<< HEAD
 wb = openpyxl.load_workbook(manage_attendance,xlsx)
+=======
+wb = openpyxl.load_workbook(filename)
+>>>>>>> 813f8af (manager_result 주석 처리)
 sheet=wb.worksheets[0]
 data = []
 for row in sheet.rows:
