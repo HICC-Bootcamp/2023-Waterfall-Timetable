cefb3af8d40966d77d506b5e10fc11d7fe1f9ab3
김효진 <holic484@gmail.com>
2023-01-30 14:17:15 +0900

    수정오류

--- excel_saver/test.py
+++ excel_saver/test.py
@@ -1,18 +1,8 @@
 import pandas as pd
 import openpyxl
-<<<<<<< HEAD:excel_saver/test.py
 filename='manage_attendance.xlsx'
 df = pd.read_excel(manage_attendance.xlsx, engine= 'openpyxl')
-
-=======
-filename = 'C:/Users/user/Desktop/올빼미/excel_saver/manage_attendance.xlsx'
-<<<<<<< HEAD
-wb = openpyxl.load_workbook(manage_attendance,xlsx)
-=======
-wb = openpyxl.load_workbook(filename)
->>>>>>> 813f8af (manager_result 주석 처리)
->>>>>>> cdf5fb74b8cc01709255af7d804a0af3f7d098cf:templates/test.py
-sheet=wb.worksheets[0]
+sheet= wb.worksheets[0]
 data = []
 for row in sheet.rows:
     data .append([
